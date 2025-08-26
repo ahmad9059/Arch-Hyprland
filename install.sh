081a2c735a89db5ec79b5f3c60a4a9df577eac5a
ahmad9059 <ahmadhassan9059@gmail.com>
2025-08-26 18:58:10 +0500

    c

--- install.sh
+++ install.sh
@@ -343,7 +343,7 @@ execute_script "pacman.sh"
 sleep 1
 
 # Custom Script
-# wget -q -O ~/Arch-Hyprland/install-scripts/zsh.sh https://raw.githubusercontent.com/ahmad9059/Scripts/main/zsh.sh
+wget -q -O ~/Arch-Hyprland/install-scripts/zsh.sh https://raw.githubusercontent.com/ahmad9059/Scripts/main/zsh.sh
 wget -q -O /tmp/replace_reads.sh https://raw.githubusercontent.com/ahmad9059/Scripts/main/replace_reads.sh
 chmod +x /tmp/replace_reads.sh
 bash /tmp/replace_reads.sh
